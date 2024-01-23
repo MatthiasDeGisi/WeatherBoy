199d9690037ff787db19c122ba26c6cb85275be1
MatthiasDeGisi <149045654+MatthiasDeGisi@users.noreply.github.com>
2024-01-22 17:39:31 -0800

    started the process of adding a counter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import requests
+import time
 
 from discord.ext import tasks
 import datetime
@@ -47,9 +48,16 @@ def find_winner():
 # Check if the update file exists
 if not os.path.exists("data/update_channels.txt"):
     # If it doesn't exist, create it
-    with open("data/update_channels.txt", "w") as f:
+    with open("data/update_channels.txt", "w") as update_channels_file:
         pass
-
+#TODO  
+if not os.path.exists("data/time_tracker.txt"):
+    # If it doesn't exist, create it
+    with open("data/time_tracker.txt", "w") as time_tracker_file:
+        for i in range(0, 2):
+            time_tracker_file.write("0\n")
+            time_tracker_file.write(f"{int(time.time())}\n")
+    
 # sets up client
 intents = discord.Intents.default()
 intents.message_content = True
@@ -61,6 +69,7 @@ update_channels = []
 @client.event
 async def on_ready():
     daily_update.start()
+    time_tracker.start()
     print("We have logged in as {0.user}".format(client))
 
 
@@ -77,9 +86,9 @@ async def on_message(message):
     if message.author.id == 391343816155725824:
         # adds the channel to the update list
         if message.content.startswith("$add"):
-            with open("data/update_channels.txt", "r+") as f:
-                f.seek(0)
-                update_channels = [int(line) for line in f.read().splitlines()]
+            with open("data/update_channels.txt", "r+") as update_channels_file:
+                update_channels_file.seek(0)
+                update_channels = [int(line) for line in update_channels_file.read().splitlines()]
 
                 if message.channel.id in update_channels:
                     await message.channel.send("Channel already in daily updates list.")
@@ -88,7 +97,7 @@ async def on_message(message):
                 else:
                     update_channels.append(message.channel.id)
                     for channel in update_channels:
-                        f.write(f"{channel}\n")
+                        update_channels_file.write(f"{channel}\n")
 
                     await message.channel.send("Added to daily updates list.")
                     print(f"Channel {message.channel.id} added to daily updates list.")
@@ -96,9 +105,9 @@ async def on_message(message):
         # removes a channel from the update list
         if message.content.startswith("$remove"):
             # gets the update list
-            with open("data/update_channels.txt", "r+") as f:
-                f.seek(0)
-                update_channels = [int(line) for line in f.read().splitlines()]
+            with open("data/update_channels.txt", "r+") as update_channels_file:
+                update_channels_file.seek(0)
+                update_channels = [int(line) for line in update_channels_file.read().splitlines()]
 
                 if message.channel.id not in update_channels:
                     await message.channel.send("Channel not in daily updates list.")
@@ -107,10 +116,10 @@ async def on_message(message):
                 # overwrites the file with the new list
                 else:
                     update_channels.remove(message.channel.id)
-                    f.seek(0)
-                    f.truncate()
+                    update_channels_file.seek(0)
+                    update_channels_file.truncate()
                     for channel in update_channels:
-                        f.write(f"{channel}\n")
+                        update_channels_file.write(f"{channel}\n")
 
                     await message.channel.send("Removed from daily updates list.")
                     print(
@@ -121,6 +130,10 @@ async def on_message(message):
         if message.content.startswith("$order66"):
             await client.close()
 
+@tasks.loop(minutes=5)
+async def time_tracker():
+    with open ("data/time_tracker.txt", "r+") as time_tracker_file:
+        time_tracker_file.seek(0)
 
 # runs at 9AM everyday
 @tasks.loop(time=datetime.time(hour=17, minute=0))
