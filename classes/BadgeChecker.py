2452c353c84b1b5d13ff7e3c3d5c8927ed84805a
Matthias De Gisi <matthias.degisi@gmail.com>
2024-12-22 14:40:27 -0800

    fleshed out adding and deleting stations

--- classes/BadgeChecker.py
+++ classes/BadgeChecker.py
@@ -30,9 +30,7 @@ class BadgeChecker:
         # Iterates through the documents and grabs the station ID from each one.
         for doc in docs:
             # Converts the document to a dictionary.
-            dict = (
-                doc.to_dict()
-            ) 
+            dict = doc.to_dict()
             owner_first_name = dict["OwnerFirstName"]
             station_id = dict["StationID"]
 
@@ -40,24 +38,44 @@ class BadgeChecker:
 
         return station_ids
 
-    def add_station(self, station_id: str, owner_name: str) -> None:
+    def add_station(
+        self, station_id: str, owner_first_name: str, owner_last_name: str
+    ) -> None:
         """Add a station to the Firestore database.
 
         Args:
             station_id (str): The station ID.
-            owner_name (str): The owner's name.
-        """
+            owner_first_name (str): The owner's first name.
+            owner_last_name (str): The owner's last name.
+        """ ""
         pass
-    
+        new_station = {
+            "StationID": station_id,
+            "OwnerFirstName": owner_first_name,
+            "OwnerLastName": owner_last_name,
+        }
+
+        self.db.collection("Stations").add(new_station)
+
     def remove_station(self, station_id: str) -> None:
         """Remove a station from the Firestore database.
 
         Args:
             station_id (str): The station ID.
         """
-        pass
-    
-    
+        # Grabs the document from the Stations collection that matches the station ID. Uses .get() 
+        # instead of .stream() because there should be very few documents.
+        # I have to use .get() and retrieve the whole document instead of just the reference. This is
+        # a limitation of the SDK.
+        docs = self.db.collection("Stations").where("StationID", "==", station_id).get()
+        
+        for doc in docs:
+            # I guess that with a document, you can call .reference to get the reference to the document which is then used for deletion.
+            doc.reference.delete()
+
+
 if __name__ == "__main__":
     checker = BadgeChecker()
+    checker.add_station("123", "John", "Doe")
     print(checker.get_stations())
+    checker.remove_station("123")
