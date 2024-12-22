a14a704150643aa6287f3b56f48aacb35523aa5a
Matthias De Gisi <matthias.degisi@gmail.com>
2024-12-21 22:52:13 -0800

    started using a package structure

---
+++ classes/__init__.py
@@ -0,0 +1,7 @@
+'''This file exposes the classes in the classes folder so that they can be imported directly by name in other files.
+e.g:
+from classes import BadgeChecker
+instead of:
+from classes.BadgeChecker import BadgeChecker
+'''
+from .BadgeChecker import BadgeChecker
