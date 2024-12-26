0792e8e75d2623944ff6524c63ebcf2ebb9fe722
Matthias De Gisi <matthias.degisi@gmail.com>
2024-12-26 00:41:27 -0800

    started the groundwork for slash commands

--- main.py
+++ main.py
@@ -24,31 +24,47 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 # This is a subclass of client that provides a command processing system.
-# The command prefix is the character that triggers the command, the intents 
+# The command prefix is the character that triggers the command, the intents
 # are the intents that the bot will use, and the application_id is required
 # for the bot to sync slash commands (https://discordpy.readthedocs.io/en/stable/interactions/api.html#discord.app_commands.CommandTree.sync).
-bot = commands.Bot(command_prefix='/', intents=intents, application_id=int(os.getenv("APPLICATION_ID")))
-
+bot = commands.Bot(
+    command_prefix="/", intents=intents, application_id=int(os.getenv("APPLICATION_ID"))
+)
 
 
 @bot.event
 async def on_ready():
-    # Sync the slash commands I think?
-    # await bot.tree.sync()
+    # Sync the slash commands with discord.
     try:
         synced_commands = await bot.tree.sync()
-        print(f"Synced {len(synced_commands)} command(s)")
+        print(
+            f"Synced {len(synced_commands)} command(s)"
+        )  # If the sync is successful, this will print the number of commands synced.
     except Exception as e:
         print(f"Commands could not be synced: {e}")
-        
-    print(f'We have logged in as {bot.user}')
 
-# This is a decorator that registers a new command under the bot. the .tree part 
+    print(f"We have logged in as {bot.user}")
+
+
+# This is a decorator that registers a new command under the bot. the .tree part
 # is needed for slash commands (app commands), as opposed to a regular prefix command.
 # https://discordpy.readthedocs.io/en/stable/interactions/api.html#discord.app_commands.Command
 # https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.Command
-@bot.tree.command()
-async def badge(ctx, station_id: str, first_name: str, last_name: str):
-    await ctx.send(f"{station_id}, {first_name}, {last_name}")
+# name=... is the name of the command that will be used in Discord.
+@bot.tree.command(name="addstation")
+async def add_station(
+    interaction: discord.Interaction,
+    station_id: str,
+    first_name: str,
+    last_name: str,  # These names must be all lowercase. Not exactly sure how come, just that discord.py throws me error codes..
+):
+    try:
+        checker.add_station(station_id, first_name, last_name)
+        await interaction.response.send_message(
+            f"Station {station_id} added with owner {first_name} {last_name}."
+        )
+    except Exception as e:
+        await interaction.response.send_message(f"Error adding station: {e}")
+
 
 bot.run(os.getenv("TOKEN"))
