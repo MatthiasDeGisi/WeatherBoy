a93bbb871d9be2251063e890dc43bee14f5a28d8
Matthias De Gisi <matthias.degisi@gmail.com>
2024-12-28 20:35:30 -0800

    set up the bot to use cogs

---
+++ cogs/CheckBadge.py
@@ -0,0 +1,75 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+# Import custom badgechecker class.
+from classes import BadgeChecker
+
+# Importing the datetime modules for calculating the time.
+import datetime
+from datetime import timedelta
+
+class CheckBadge(commands.Cog):
+    def __init__(self, bot):
+        """Initializes the CheckBadge cog.
+
+        Args:
+            bot (commands.Bot): The bot that the cog is loaded into.
+        """
+        # This is a reference to the bot object that is passed in when the cog is loaded.
+        self.bot = bot # May not be needed
+        
+        # Create a new BadgeChecker object.
+        self.checker = BadgeChecker()
+        
+        # Set the time for the daily update.
+        self.daily_update_time = datetime.time(9, 0)
+
+    # This is a decorator that registers a new command under the bot. the .tree part
+    # is needed for slash commands (app commands), as opposed to a regular prefix command.
+    # https://discordpy.readthedocs.io/en/stable/interactions/api.html#discord.app_commands.Command
+    # https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.Command
+    # name=... is the name of the command that will be used in Discord.
+    @app_commands.command(name="addstation")
+    async def add_station(
+        self,
+        interaction: discord.Interaction,
+        station_id: str,
+        first_name: str,
+        last_name: str,  # These names must be all lowercase. Not exactly sure how come, just that discord.py throws me error codes.
+    ):
+        """Command to add a station to the Firestore database.
+
+        Args:
+            interaction (discord.Interaction): This is passed in by the discord.py library and is used to send messages back to the user.
+            station_id (str): The ID of the station to add.
+            first_name (str): First name of the station owner.
+            last_name (str): Last name of the station owner.
+        """
+        try:
+            self.checker.add_station(station_id, first_name, last_name)
+            await interaction.response.send_message(
+                f"Station {station_id} added with owner {first_name} {last_name}."
+            )
+        except Exception as e:
+            await interaction.response.send_message(f"Error adding station: {e}")
+
+    @app_commands.command(name="removestation")
+    async def remove_station(self, interaction: discord.Interaction, station_id: str):
+        """Command to remove a station from the Firestore database.
+
+        Args:
+            interaction (discord.Interaction): This is passed in by the discord.py library and is used to send messages back to the user.
+            station_id (str): The ID of the station to remove.
+        """
+        try:
+            self.checker.remove_station(station_id)
+            await interaction.response.send_message(f"Station {station_id} removed.")
+        except Exception as e:
+            await interaction.response.send_message(f"Error removing station: {e}")
+    
+    
+
+async def setup(bot):
+    """Setup function to add the cog as an extension. is used by the bot.load_extension() function. Must be async."""
+    await bot.add_cog(CheckBadge(bot))
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Import custom badgechecker class.
-from classes import BadgeChecker
-
 # Importing the discord library, the commands module, and the tasks module from discord.ext.
 import discord
 from discord.ext import tasks
@@ -11,19 +8,16 @@ import os
 from dotenv import load_dotenv
 
 # Importing the datetime modules for calculating the time.
-import datetime
-from datetime import timedelta
+# import datetime
+# from datetime import timedelta
 
 load_dotenv()
-
-# Creating an instance of the BadgeChecker class.
-checker = BadgeChecker()
 
 # Setting up the client with intents.
 intents = discord.Intents.default()
 intents.message_content = True
 
-# This is a subclass of client that provides a command processing system.
+# This is a subclass of client that provides a command tree automatically.
 # The command prefix is the character that triggers the command, the intents
 # are the intents that the bot will use, and the application_id is required
 # for the bot to sync slash commands (https://discordpy.readthedocs.io/en/stable/interactions/api.html#discord.app_commands.CommandTree.sync).
@@ -34,6 +28,16 @@ bot = commands.Bot(
 
 @bot.event
 async def on_ready():
+    # Load all the cogs in the cogs folder.
+    try:
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                await bot.load_extension(
+                    f"cogs.{filename[:-3]}"
+                )  # Strips the file extension.
+    except Exception as e:
+        print(f"Error loading extension: {e}")
+
     # Sync the slash commands with discord.
     try:
         synced_commands = await bot.tree.sync()
@@ -44,27 +48,6 @@ async def on_ready():
         print(f"Commands could not be synced: {e}")
 
     print(f"We have logged in as {bot.user}")
-
-
-# This is a decorator that registers a new command under the bot. the .tree part
-# is needed for slash commands (app commands), as opposed to a regular prefix command.
-# https://discordpy.readthedocs.io/en/stable/interactions/api.html#discord.app_commands.Command
-# https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.Command
-# name=... is the name of the command that will be used in Discord.
-@bot.tree.command(name="addstation")
-async def add_station(
-    interaction: discord.Interaction,
-    station_id: str,
-    first_name: str,
-    last_name: str,  # These names must be all lowercase. Not exactly sure how come, just that discord.py throws me error codes..
-):
-    try:
-        checker.add_station(station_id, first_name, last_name)
-        await interaction.response.send_message(
-            f"Station {station_id} added with owner {first_name} {last_name}."
-        )
-    except Exception as e:
-        await interaction.response.send_message(f"Error adding station: {e}")
 
 
 bot.run(os.getenv("TOKEN"))
